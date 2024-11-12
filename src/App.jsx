2b8ee7bec8fe1eaef6c121457c157d3296334fec
yoshixi <davidchenyuhe@proton.me>
2024-11-11 23:32:31 -0500

    fix the xs

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Hello from './components/hello'
-import Message from './components/message'
-import Form from './components/Form'
-import Search from './components/Search'
 import Card from './components/Card'
 import Light from './components/Light'
 
 function App() {
-  const [light, setLight] = useState(false);
 
   return (
     <>
-     <div className={`${light ? 'bg-black' : 'bg-white'} flex flex-row h-screen w-screen place-content-center`}> 
-      <Light light={light} setLight={setLight}></Light>
-      <Card name='daniel' profession='eater' desc='he is p cool'></Card>
       <Card ></Card>
-    </div>
+      <Light></Light>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,14 @@
 import reactLogo from '../assets/react.svg'
 
 
-function Card({name='person', profession='human', desc='anything at all'}){
+function Card(){
     return (
-        <div className="drop-shadow-lg flex flex-col rounded-md m-[10px] w-[15%] h-[20%] bg-[#ff9922ff]">
-            <div className="flex flex-row">
-            <img src={reactLogo} className="w-[70px] h-[70px]"/>
-            <div className="flex flex-col">
-                <p className="relative left-2 text-3xl">{name}</p>
-                <p className="relative left-2 text-2xl">{profession}</p>
+        <div>
+            <img src={reactLogo}/>
+            <div >
+                <p> Name </p>
+                <p> Description</p>
             </div>
-            </div>
-                <p className="relative text-1xl top-2 left-2">{desc}</p>
         </div>
     )
 }
